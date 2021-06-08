f9d6437a620d4419ba038992da6aec8b7852437f
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-06-08 17:47:26 +0300

    tasks 08 on tutorial

--- src/js/gallery.js
+++
@@ -1,6 +0,0 @@
-import galleryCards from '../templets/gallery-cards.hbs';
-import menu from '../menu.json';
-
-const listOfMenu = document.querySelector('.js-menu');
-
-listOfMenu.insertAdjacentHTML('beforeend', galleryCards(menu));
